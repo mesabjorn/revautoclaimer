806b670f0b4baa94c85e9cc41bffdea56c384266
bjornvdster <bjornv_dster@outlook.com>
2021-09-21 12:20:17 +0200

    rewrote claiming algo

--- rev.js
+++ rev.js
@@ -1,6 +1,7 @@
 class RevObserver{
 	constructor(){
-		this.observing = []
+		this.observing = [];
+		this.claim = false;
 	}
 	
 	AddObserverToElement(t){
@@ -8,37 +9,70 @@ class RevObserver{
 			mutations.forEach((m) => {
 				if(m.addedNodes.length>0){
 					var node = m.addedNodes[0];
-					if(node.nodeName=="A"){
+					if(node.nodeName==="A"){
 						//console.log({"node":node} );
 						//console.log("Found new jobs..clicking on "+node);
 						node.click();
 					}
 				}
-				if(t=="table find-work-projects"){
-					//console.log("observed mutation in target "+t);
-					//console.log({m:m,t:t});
-					if(m.type=="childList"){return}
-					let projects = document.getElementsByClassName("find-work-row");
-					for(let i = 1;i< projects.length;i++){
-						//p=projects[i];		
-						let proj = {project:projects[i].innerText.split("\n")};
-						this.addProject(proj);
-						console.log({project:proj});						 
-						projects[i].childNodes[0].click()		//click project
+				if(t==="table find-work-projects"){
+					if(document.getElementsByClassName("virtuoso-container").length===0){return}					
+					if(m.addedNodes.length>0){
+						//console.log("Node added to target "+t);
+						//console.log({m:m,t:t});					
+//						if(m.type==="childList"){return}
+						if(m.addedNodes[0].childNodes.length===0){return}
+						if(m.addedNodes[0].childNodes[0].className==="table-row"){
+							//console.log(m.addedNodes[0]);					
+
+							let projectNodeRow = m.addedNodes[0].childNodes[0];
+							let project = projectNodeRow.innerText.split("\n");
+														
+							this.addProject({project});
+							
+							projectNodeRow.getElementsByClassName("project-info")[0].click();
+							
+							setTimeout(()=>{
+								//console.log(this);
+								if(this.claim){
+									let btn = document.getElementsByClassName("btn btn-std project-claim-btn")[0];
+									console.log("clicked claim button:",btn);
+									
+									//document.getElementsByClassName("btn btn-std project-claim-btn")[0].click(); //claim it after 2.5s							
+								}
+							},2500);
+							
+						}						
 						
-						setTimeout(()=>{							
-							document.getElementsByClassName("btn btn-std project-claim-btn")[0].click(); //claim it after 2.5s							
-						},2500);
+						/*
+						let projects = document.getElementsByClassName("find-work-row");
+						for(let i = 1;i< projects.length;i++){
+							
+							let proj = {project:projects[i].innerText.split("\n")};
+							this.addProject(proj);
+							console.log({project:proj});						 
+							projects[i].childNodes[0].click()		//click project
+							
+							setTimeout(()=>{
+								console.log(this);
+								if(this.claim){
+									//document.getElementsByClassName("btn btn-std project-claim-btn")[0].click(); //claim it after 2.5s							
+								}
+							},2500);
+							
+							//document.getElementsByClassName("btn btn-std project-claim-btn")[0].click(); //claim it
+							
 						
-						//document.getElementsByClassName("btn btn-std project-claim-btn")[0].click(); //claim it
-						
-						
+							
+							
+						}
+						*/
 					}
 				}
 			});
 		});
 		let target = document.getElementsByClassName(t)[0];
-		let observerConfig = {subtree: true, childList: true,characterData:true};
+		let observerConfig = {subtree: true, childList: true};//,characterData:true};
 		observer.observe(target, observerConfig);
 		this.observing.push({observer:observer,target:target});
 	}
@@ -54,6 +88,7 @@ class RevObserver{
 			let projects = items.projects;
 			p.date=new Date().toLocaleString();
 			projects.push(p);
+			console.log("Added project: ",p)
 			chrome.storage.local.set({projects:projects}, function(){
 				console.log(`Wrote ${projects.length} project(s) to storage.`);
 			});
@@ -62,7 +97,25 @@ class RevObserver{
 }
 
 o = new RevObserver();
-o.AddObserverToElement("projects-banner");
+//o.AddObserverToElement("projects-banner");
 o.AddObserverToElement("table find-work-projects");
+
+
+target=document.getElementsByTagName("h5")[0];
+observerConfig = {subtree: true, childList: true,characterData:true};
+
+obs3 = new MutationObserver((mutations) => {
+	console.log(mutations);
+	mutations.forEach((m) => {
+		if(m.type==="characterData"){
+			let text = m.target.textContent;
+			console.log(text);
+			if(text.indexOf("Click here to get the latest")>-1){
+				target.click();				
+			}
+		}
+	});
+});
+obs3.observe(target, observerConfig);
 
 console.log("Added rev crawler!")
